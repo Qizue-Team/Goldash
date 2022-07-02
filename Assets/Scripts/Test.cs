7491f5e9afe8bcfc9dff10cd2174ce67263fd520
MicheleBuonocunto <108434192+MicheleBuonocunto@users.noreply.github.com>
2022-07-02 12:18:38 +0200

    Update test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int ciao;
+
+
+    // Start is called before the first frame update 
     void Start()
     {
         
