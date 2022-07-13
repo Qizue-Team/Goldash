e7a5bb440f6a5067b872372be1ff52b729fa5b35
Cristian <cri.ghiorzi@gmail.com>
2022-07-13 11:20:09 +0200

    Overheating events

--- Assets/Scripts/Player/PlayerOverheat.cs
+++ Assets/Scripts/Player/PlayerOverheat.cs
@@ -10,12 +10,20 @@ public class PlayerOverheat : MonoBehaviour
     [Header("References")]
     [SerializeField]
     private GameObject overheatMask;
+    [SerializeField]
+    private TerrainTileSpawner terrainTileSpawner;
 
     [Header("Settings")]
     [SerializeField]
     private float heatIncreaseAmount = 0.1f;
     [SerializeField]
     private float heatDecreaseAmount = 0.1f;
+    [SerializeField]
+    private float slowDownLevel = 0.7f;
+    [SerializeField]
+    private float slowDownMultiplier = 1.5f;
+
+    private bool _isSlowedDown = false;
 
     public void IncreaseHeat()
     {
@@ -39,7 +47,21 @@ public class PlayerOverheat : MonoBehaviour
 
         CustomLog.Log(CustomLog.CustomLogType.PLAYER, "Overheat: " + Overheat);
 
+        if(Overheat >= slowDownLevel && (!_isSlowedDown))
+        {
+            CustomLog.Log(CustomLog.CustomLogType.PLAYER, "Slow Down Triggered ");
+            terrainTileSpawner.SpeedDown(slowDownMultiplier);
+            _isSlowedDown = true;
+        }
+
         overheatMask.transform.localPosition = new Vector3(overheatMask.transform.localPosition.x, -(1.0f - Overheat), 0.0f);
+
+        if(Overheat >= 1)
+        {
+            // GameOver - TODO: CALL GAMEOVER EVENT
+            CustomLog.Log(CustomLog.CustomLogType.PLAYER, "GAME OVER for Overheating");
+            Destroy(gameObject);
+        }
     }
 
     public void DecreaseHeat(float amount)
@@ -53,6 +75,14 @@ public class PlayerOverheat : MonoBehaviour
             Overheat = 0;
 
         CustomLog.Log(CustomLog.CustomLogType.PLAYER, "Overheat: " + Overheat);
+
+
+        if (Overheat < slowDownLevel && _isSlowedDown)
+        {
+            CustomLog.Log(CustomLog.CustomLogType.PLAYER, "Speed to normal ");
+            terrainTileSpawner.SpeedUp(slowDownMultiplier);
+            _isSlowedDown = false;
+        }
 
         overheatMask.transform.localPosition = new Vector3(overheatMask.transform.localPosition.x, -(1.0f-Overheat), 0.0f);
     }
