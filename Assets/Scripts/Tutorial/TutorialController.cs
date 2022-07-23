c0c271c8fed1e5cb615c2ecd620d675e00350cc8
Cristian <cri.ghiorzi@gmail.com>
2022-07-23 14:33:00 +0200

    Tutorial Controller Setup

--- Assets/Scripts/Tutorial/TutorialController.cs
+++ Assets/Scripts/Tutorial/TutorialController.cs
@@ -6,9 +6,41 @@ public class TutorialController : Singleton<TutorialController>
 {
     [Header("References")]
     [SerializeField]
+    private PlayerJump playerJump;
+    [SerializeField]
     private TutorialSpawner tutorialSpawner;
     [SerializeField]
     private TerrainTileSpawner tileSpawner;
 
-    
+    private GameObject enemyTarget;
+
+    private void Start()
+    {
+        StartCoroutine(COExecuteTutorialJump());
+    }
+
+    private void Update()
+    {
+        if(enemyTarget != null && Mathf.Abs(playerJump.gameObject.transform.position.x - enemyTarget.transform.position.x) <= 2.0f)
+        {
+            StopTutorial();
+        }
+    }
+
+    private void StopTutorial()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    private void ResumeTutorial()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    private IEnumerator COExecuteTutorialJump()
+    {
+        playerJump.SetJumpActive(false);
+        yield return new WaitForSeconds(2.0f);
+        enemyTarget = tutorialSpawner.SpawnEnemy();
+    }
 }
