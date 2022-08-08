9960ee2b221472d9bdbf5aa45063317775fe155f
Cristian <cri.ghiorzi@gmail.com>
2022-08-08 10:48:22 +0200

    RecapPanel script

--- Assets/Scripts/UI/RecapPanel/RecapPanel.cs
+++ Assets/Scripts/UI/RecapPanel/RecapPanel.cs
@@ -1,8 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RecapPanel : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField]
+    private GearEntry gearEntry;
+    [SerializeField]
+    private ScoreEntry scoreEntry;
+    [SerializeField]
+    private Transform trashContentList;
+    [SerializeField]
+    private GameObject trashEntryPrefab;
 
+    public void AddTrashEntry(string name, int quantity, int score, int gear)
+    {
+        GameObject entryObj = Instantiate(trashEntryPrefab, trashContentList);
+        TrashEntry entry = entryObj.GetComponent<TrashEntry>();
+        entry.SetEntry(name, quantity, score, gear);
+    }
+
+    public void SetGearEntry(int gear)
+    {
+        gearEntry.SetEntry(gear);
+    }
+
+    public void SetScoreEntry(int total, int trash, int enemy, int distance)
+    {
+        scoreEntry.SetEntry(total, trash, enemy, distance);
+    }
 }
