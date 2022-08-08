42939eac01f37515a5ea7ca1ebbcef5a7c9769eb
Cristian <cri.ghiorzi@gmail.com>
2022-08-08 10:57:01 +0200

    Working recap panel

--- Assets/Scripts/UI/GameOverMenuPanel.cs
+++ Assets/Scripts/UI/GameOverMenuPanel.cs
@@ -1,14 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using xPoke.CustomLog;
 
 public class GameOverMenuPanel : GameMenuPanel
 {
     // Add recap menu reference here
+    [Header("GameOver References")]
+    [SerializeField]
+    private RecapPanel recapPanel;
 
     public override void Open()
     {
         base.Open();
-        // Load data here on recap menu
+
+        // Load data on recap panel
+        SetRecapPanel();
+    }
+
+    private void SetRecapPanel()
+    {
+        foreach (string name in TrashCollectedManager.Instance.TrashCountDictionary.Keys)
+        {
+            recapPanel.AddTrashEntry(name, TrashCollectedManager.Instance.TrashCountDictionary[name],
+                (TrashCollectedManager.Instance.TrashCountDictionary[name] * TrashCollectedManager.Instance.TrashDictionary[name].ScorePoints),
+                (TrashCollectedManager.Instance.TrashCountDictionary[name] * TrashCollectedManager.Instance.TrashDictionary[name].Gear));
+        }
+
+        recapPanel.SetScoreEntry(GameController.Instance.Score, 
+            TrashCollectedManager.Instance.GetTotalScoreFromTrash(),
+            EnemyKilledManager.Instance.TotalPoints, 
+            (GameController.Instance.Score - TrashCollectedManager.Instance.GetTotalScoreFromTrash() - EnemyKilledManager.Instance.TotalPoints));
+        
+        recapPanel.SetGearEntry(TrashCollectedManager.Instance.GetTotalGearCount());
     }
 }
