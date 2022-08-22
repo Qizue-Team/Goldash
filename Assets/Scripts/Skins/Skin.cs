73b404ec3b56cb97d436217943e48ecc341f24e8
Cristian <cri.ghiorzi@gmail.com>
2022-08-22 10:18:15 +0200

    Witch Hat and Sword skins prefabs created

---
+++ Assets/Scripts/Skins/Skin.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skin : MonoBehaviour
+{ 
+    public int ID { get => id; }
+    public bool IsUnlocked { get; private set; }
+    public int Cost { get => cost; }
+    public Vector3 SpawnPosition { get=>spawnPosition; }
+
+    [Header("Skin Settings")]
+    [SerializeField]
+    private int id;
+    [SerializeField]
+    private int cost;
+    [SerializeField]
+    private Vector3 spawnPosition;
+
+    public void UpdateSkin()
+    {
+        // TO-DO:
+        // Check if is already unlocked
+        // if it is unlock = true
+    }
+
+    public void Unlock()
+    {
+        if (IsUnlocked)
+            return;
+
+        int total = DataManager.Instance.LoadTotalGearCount();
+        if (total < cost)
+            return;
+        total -= cost;
+        DataManager.Instance.SaveTotalGearCount(total);
+
+        IsUnlocked = true;
+    }
+}
